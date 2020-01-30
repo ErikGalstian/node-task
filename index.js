05d8b3c5f0bfd3e9fd87dd1df598be567486ffb3
Erik Galstian <ErikGalstian94@gmail.com>
2020-01-30 19:31:43 +0400

    fixed mistakes

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const moment = require('moment');
+const stream = require('stream');
 const fs = require('fs');
 
-const writableStream = fs.createWriteStream(__dirname + '/file.txt', {
-  encoding: 'utf8'
-});
+// Getting the current time and date using Moment.js
+const data = moment().toString();
 
-setInterval(() => {
-  writableStream.write(moment().format() + '\n');
-  console.log('Date is recorded');
-}, 1000);
+// Setting up the delay between each chunk
+const delay = 1000;
+
+// Creating modified readable class with overrided _read() method
+class Readable extends stream.Readable {
+  _read() {
+    setTimeout(() => {
+      this.push(data);
+    }, delay);
+  }
+}
+
+// Creating modified transform class with overrided _transform() method
+class Transform extends stream.Transform {
+  _transform(chunk, encoding, next) {
+    chunk = chunk.toString('utf8').toLowerCase();
+    this.push(chunk);
+    next();
+  }
+}
+
+// Creating modified writable class with overrided _write method
+class Writable extends stream.Writable {
+  _write(chunk, encoding, next) {
+    fs.appendFile(__dirname + '/file.txt', chunk + '\n', err => {
+      if (err) {
+        return console.log('There was an error!');
+      }
+      console.log(`Writing this chunk of data: "${chunk}"`);
+    });
+    next();
+  }
+}
+
+// Instantiating new readable, transform and writable streams
+const readable = new Readable();
+const transform = new Transform();
+const writable = new Writable();
+
+// Setting up pipes between streams
+readable.pipe(transform).pipe(writable);
+
+// Calling the read method of readable stream
+readable.read();
